8e180a94e9a6221a0ae5e555a0e5cb8d854bd6e7
jonathan-henrique <jonathan.henrique@senior.com.br>
2020-02-19 16:48:03 -0300

    Adding customization claims at payload of JWT

--- src/main/java/com/algaworks/algafood/auth/core/AuthUser.java
+++ src/main/java/com/algaworks/algafood/auth/core/AuthUser.java
@@ -13,11 +13,13 @@ public class AuthUser extends org.springframework.security.core.userdetails.User
 		
 	private static final long serialVersionUID = 1L;
 	
+	private Long userId;
 	private String fullName;
 	
 	public AuthUser(User user) {
 		super(user.getEmail(), user.getPassword(), Collections.emptyList());
 		
+		this.userId = user.getId();
 		this.fullName = user.getName();
 	}
 
