ec475cacff4de0d8d240bc665b4b989e5e8676d1
jonathan-henrique <jonathan.henrique@senior.com.br>
2020-02-19 10:43:59 -0300

    Enable token key access for endpoint GET /oauth/token_key returning public key

--- src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -72,6 +72,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		security.checkTokenAccess("permitAll()")
+		.tokenKeyAccess("permitAll()")
 		.allowFormAuthenticationForClients();
 	}
 
